f522d354cb39fe1b0ccdedeb1510cb4759f4c707
dgusgus <dgusgus1@gmail.com>
2020-07-23 08:13:38 -0800

    patron de diseño Singleton

---
+++ sistemacompra/sistemacompra/modelo/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemacompra.modelo
+{  
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        public Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+    }
+    
+
+    
+}
